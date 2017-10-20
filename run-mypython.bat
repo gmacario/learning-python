a0d018d48955ee2bf498745dcaba14f00f8a7e55
Gianpaolo Macario <gianpaolo_macario@mentor.com>
2017-10-20 14:32:18 +0200

    Add run-mypython.bat
    Customize the script according to where you have installed
    Python in your filesystem, as well as which script to run

---
+++ run-mypython.bat
@@ -0,0 +1,10 @@
+@echo on
+
+set PYTHON_PATH=C:\Users\gmacario\AppData\Local\Programs\Python\Python36
+set WORKDIR=D:\data\github\gmacario\learning-python
+
+D:
+cd %WORKDIR%
+%PYTHON_PATH%\python lpbook\ch1\bike.py
+
+@rem EOF
