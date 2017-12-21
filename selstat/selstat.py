8f640ae45c68bb1ef76c156fa226ca3554684110
Gianpaolo Macario <gianpaolo_macario@mentor.com>
2017-12-21 08:47:32 +0000

    selstat.py: Make variables lowercase

--- selstat/selstat.py
+++ selstat/selstat.py
@@ -13,14 +13,16 @@ print("Hello, world!")
 flexelint_outdir = "/home/gmacario/tmp/linux-4.4.50-min-lint"
 linux_sourcedir = "/home/gmacario/minimized-tree-v4.4.50-bradocaj"
 
-def traverse_tree(rootDir):
+def traverse_tree(rootdir):
     """Traverse a Directory Tree
-    Reference: https://www.pythoncentral.io/how-to-traverse-a-directory-tree-in-python-guide-to-os-walk/
+    See also:
+    * https://docs.python.org/3/library/os.html
+    * https://www.pythoncentral.io/how-to-traverse-a-directory-tree-in-python-guide-to-os-walk/
     """
-    print('DEBUG: traverse_tree(rootDir=%s)' % rootDir)
-    for dirName, subdirList, fileList in os.walk(rootDir):
-        print('DEBUG: Found directory: %s' % dirName)
-        for fname in fileList:
+    print('DEBUG: traverse_tree(rootDir=%s)' % rootdir)
+    for dirpath, dirnames, filenames in os.walk(rootdir):
+        print('DEBUG: Found directory: %s' % dirpath)
+        for fname in filenames:
             print('\t%s' % fname)
 
 def main():
